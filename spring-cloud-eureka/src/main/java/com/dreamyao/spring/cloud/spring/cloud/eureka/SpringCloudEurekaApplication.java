842746c61736c5843cbc700a784a331a2767d5be
408527015 <408527015@qq.com>
2017-10-23 22:14:20 +0800

    add eureka demo

---
+++ spring-cloud-eureka/src/main/java/com/dreamyao/spring/cloud/spring/cloud/eureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.dreamyao.spring.cloud.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
