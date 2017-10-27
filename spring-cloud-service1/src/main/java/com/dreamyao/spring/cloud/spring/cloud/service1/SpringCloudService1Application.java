48fcc5fd971c778c1503a7f38ac95288053af6c2
408527015 <408527015@qq.com>
2017-10-27 21:08:06 +0800

    增加service1

---
+++ spring-cloud-service1/src/main/java/com/dreamyao/spring/cloud/spring/cloud/service1/SpringCloudService1Application.java
@@ -0,0 +1,14 @@
+package com.dreamyao.spring.cloud.spring.cloud.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudService1Application.class, args);
+	}
+}
