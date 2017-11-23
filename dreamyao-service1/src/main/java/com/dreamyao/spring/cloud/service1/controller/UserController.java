01ff5d645a236aebac0c2239ae9e960e3c42dc46
brucelong80 <408527015@qq.com>
2017-11-23 23:34:01 +0800

    mod properties and pom

--- dreamyao-service1/src/main/java/com/dreamyao/spring/cloud/service1/controller/UserController.java
+++ dreamyao-service1/src/main/java/com/dreamyao/spring/cloud/service1/controller/UserController.java
@@ -1,20 +1,21 @@
 package com.dreamyao.spring.cloud.service1.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Stone
  * @version 1.0.0
  * @date 2017/10/27
  */
-@Controller //声明为一个控制器
+@RestController
+@RequestMapping(value = "user")
 public class UserController {
 
     //处理对"/"的GET请求
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String home(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
         //视图名为home
         return "home";
     }
--- dreamyao-web/src/main/java/com/dreamyao/spring/cloud/web/DreamyaoWebApplication.java
+++ dreamyao-web/src/main/java/com/dreamyao/spring/cloud/web/DreamyaoWebApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class DreamyaoWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DreamyaoWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DreamyaoWebApplication.class, args);
+    }
 }
