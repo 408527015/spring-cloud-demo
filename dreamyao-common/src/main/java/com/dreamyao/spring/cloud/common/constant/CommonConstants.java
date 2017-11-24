05a36a34177133835390ce21d9f5a47962e1aa8c
brucelong80 <408527015@qq.com>
2017-11-25 00:06:06 +0800

    mod pom and properties

---
+++ dreamyao-common/src/main/java/com/dreamyao/spring/cloud/common/constant/CommonConstants.java
@@ -0,0 +1,17 @@
+package com.dreamyao.spring.cloud.common.constant;
+
+/**
+ * @author Stone
+ * @version 1.0.0
+ * @date 2017/11/24
+ */
+public class CommonConstants {
+
+    public static final Integer EX_TOKEN_ERROR_CODE = 40101;
+    // 用户token异常
+    public static final Integer EX_USER_INVALID_CODE = 40102;
+    // 客户端token异常
+    public static final Integer EX_CLIENT_INVALID_CODE = 40131;
+    public static final Integer EX_CLIENT_FORBIDDEN_CODE = 40331;
+    public static final Integer EX_OTHER_CODE = 500;
+}
